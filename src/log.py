5422bb14ae3f4bc0fb1b96b75b6d36ad32d4de18
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-06-05 20:40:41 +0000

    fix: dont break dev logging

--- src/log.py
+++ src/log.py
@@ -7,20 +7,21 @@ import colorlog
 
 def setup_logging():
     """Setup logging configuration if none is present."""
-    lvl = "%(log_color)s%(levelname)-4s%(reset)s"
-    s = "%(bold_light_blue)s|%(reset)s"
-    tm = "%(thin_cyan)s%(asctime)s%(reset)s"
-    nm = "%(thin_cyan)s%(name)-20s%(reset)s"
-    msg = "%(light_white)s%(message)s"
-    colorlog.basicConfig(
-        format=f"{lvl}{s}{tm}{s}{nm} {msg}",
-        datefmt="%H:%M:%S",
-        force=False,
-    )
-    logging.addLevelName(logging.DEBUG, "DBG")
-    logging.addLevelName(logging.INFO, "INFO")
-    logging.addLevelName(logging.WARNING, "WARN")
-    logging.addLevelName(logging.ERROR, "ERR")
-    logging.addLevelName(logging.CRITICAL, "CRIT")
+    if not logging.getLogger().hasHandlers():
+        lvl = "%(log_color)s%(levelname)-4s%(reset)s"
+        s = "%(bold_light_blue)s|%(reset)s"
+        tm = "%(thin_cyan)s%(asctime)s%(reset)s"
+        nm = "%(thin_cyan)s%(name)-20s%(reset)s"
+        msg = "%(light_white)s%(message)s"
+        colorlog.basicConfig(
+            format=f"{lvl}{s}{tm}{s}{nm} {msg}",
+            datefmt="%H:%M:%S",
+            force=False,
+        )
+        logging.addLevelName(logging.DEBUG, "DBG")
+        logging.addLevelName(logging.INFO, "INFO")
+        logging.addLevelName(logging.WARNING, "WARN")
+        logging.addLevelName(logging.ERROR, "ERR")
+        logging.addLevelName(logging.CRITICAL, "CRIT")
     logging.getLogger("__main__").setLevel(logging.DEBUG)
     logging.getLogger("src").setLevel(logging.DEBUG)
